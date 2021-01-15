9de05e6ab1538abcd6d576d5d8bb7f91d4247ebe
pankajmahur <pankajmahur@PANKAJ3150211>
2021-01-16 01:59:14 +0530

    Added web aggregator.

---
+++ src/APIGateways/Web/YCompany.Web.HttpAggregator/WebHttpAggregator.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Ocelot.Middleware;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YCompany.Web.HttpAggregator
+{
+    internal class WebHttpAggregator : IWebHttpAggregator
+    {
+        public Task<DownstreamResponse> Aggregate(List<HttpContext> responses)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/APIGateways/Web/YCompany.WebGateway/ServiceCollectionExt.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YCompany.WebGateway
+{
+    static class ServiceCollectionExt
+    {
+        /// <summary>
+        /// AddHttpAggregators
+        /// </summary>
+        /// <param name="services">container services</param>
+        public static void AddHttpAggregators(this IServiceCollection services)
+        {
+            //add all aggregators
+        }
+    }
+}
