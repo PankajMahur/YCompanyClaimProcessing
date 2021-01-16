dbcde6a5478d48f8741cf7b2cf51e15fd29f2ef4
pankajmahur <pankajmahur@PANKAJ3150211>
2021-01-16 18:03:40 +0530

    Updated dependecy resolver for event bus core.

---
+++ src/EventBus/YCompany.Microservice.RabbitMQ.Infra.IOC/DependencyResolver.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using YCompany.Library.MicroRabbit.Core.Bus;
+using YCompany.Library.RabbitMQ.Infra.Bus;
+
+namespace YCompany.Library.RabbitMQ.Infra.IOC
+{
+    public class DependencyResolver
+    {
+        public static void RegisterEventBusServices(IServiceCollection services)
+        {
+            //Domain bus
+            services.AddTransient<IEventBus, RabbitMQBus>();
+        }
+    }
+}
